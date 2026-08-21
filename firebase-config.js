ee26261c18c930257a00e8361549aa10ab4be316
Valério Barbosa <valerio.direito@gmail.com>
2026-08-21 19:18:27 -0300

    Ativa projeto Firebase da Trilha

--- firebase-config.js
+++ firebase-config.js
@@ -1,3 +1,10 @@
-// Preencha com a configuração do aplicativo Web criado no Firebase Console.
-// Esta configuração identifica o projeto; as regras do Firestore protegem os dados.
-globalThis.TrilhaFirebaseConfig = null;
+// Configuração pública do aplicativo Web. A segurança dos dados é aplicada
+// pelas regras do Firestore e pela autenticação, não por estes identificadores.
+globalThis.TrilhaFirebaseConfig = {
+  apiKey: "AIzaSyD79PFaTXTUJBYJYEj1YfYJnfi6lh6DerY",
+  authDomain: "trilha-flashcard.firebaseapp.com",
+  projectId: "trilha-flashcard",
+  storageBucket: "trilha-flashcard.firebasestorage.app",
+  messagingSenderId: "518682359958",
+  appId: "1:518682359958:web:e9b1f43b3765046f28aab7",
+};
