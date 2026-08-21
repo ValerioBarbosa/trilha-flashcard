1ef58e5788bdc167c228594a6c76e2fdfe5a61a3
Valério Barbosa <valerio.direito@gmail.com>
2026-08-21 19:25:08 -0300

    Corrige configuração do Firebase

--- app.js
+++ app.js
@@ -1677,7 +1677,7 @@ document.addEventListener("keydown", (event) => {
 if ("serviceWorker" in navigator && location.protocol.startsWith("http")) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
-      .register("sw.js?v=20260821-8", { updateViaCache: "none" })
+      .register("sw.js?v=20260821-9", { updateViaCache: "none" })
       .then((registration) => registration.update())
       .catch(() => {});
   });
