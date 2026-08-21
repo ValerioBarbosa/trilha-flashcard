0ac2e0d652c333167f4d418a06892a14250f1746
Valério Barbosa <valerio.direito@gmail.com>
2026-08-21 18:41:48 -0300

    Prepara sincronização segura na nuvem

---
+++ firebase-config.js
@@ -0,0 +1,3 @@
+// Preencha com a configuração do aplicativo Web criado no Firebase Console.
+// Esta configuração identifica o projeto; as regras do Firestore protegem os dados.
+globalThis.TrilhaFirebaseConfig = null;
