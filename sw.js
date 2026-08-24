7023d6864470950c3569e40dbb70ea01f767a683
Valério Barbosa <valerio.direito@gmail.com>
2026-08-24 05:25:58 -0300

    Persiste sessão ativa: sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "trilha-flashcard-v27";
+const CACHE_NAME = "trilha-flashcard-v28";
 const CORE_ASSETS = [
   "./",
   "./index.html",
   "./styles.css?v=20260824-4",
-  "./app.js?v=20260824-6",
+  "./app.js?v=20260824-7",
   "./card-import.js?v=20260821-1",
   "./card-storage.js?v=20260823-1",
   "./card-manager.js?v=20260823-1",
