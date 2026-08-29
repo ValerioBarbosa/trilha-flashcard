6e9eb88124ed7f42fa310fca6a78f1f0664ead26
Valério Barbosa <valerio.direito@gmail.com>
2026-08-29 17:56:00 -0300

    chore: remover wrapper não utilizado do banco

--- modern/src/questions/ProductionQuestionsPage.tsx
+++
@@ -1,6 +0,0 @@
-import type { User } from '@supabase/supabase-js';
-import { QuestionBankPreview } from './QuestionBankPreview';
-
-export function ProductionQuestionsPage({ user, profileId }: { user: User; profileId: string }) {
-  return <QuestionBankPreview user={user} profileId={profileId} embedded />;
-}
