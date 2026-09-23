8736eacef588b92da261ef999a65c644e1ad783b
Valério Barbosa <valerio.direito@gmail.com>
2026-09-23 04:44:53 -0300

    feat: transformar Edital em centro de comando do TRT-4
    * feat: add per-topic edital progress metrics
    
    * feat: turn Edital into study command center
    
    * feat: connect Edital actions to Lei Seca
    
    * style: add Edital command center layout

--- modern/src/app/ModernWorkspace.tsx
+++ modern/src/app/ModernWorkspace.tsx
@@ -88,7 +88,7 @@ export function ModernWorkspace({ user, onSignOut }: Props) {
           <>
             {page === 'home' ? <HomePage user={user} workspace={workspace} onNavigate={selectPage} onStudySubject={(subjectId) => focusStudyTopic(subjectId, 'all')} onReviewDue={startDueReview} /> : null}
             {page === 'study' ? <StudyPage user={user} profileId={workspace.profile.id} subjects={workspace.subjects} topics={workspace.topics} decks={workspace.decks} focus={studyFocus} /> : null}
-            {page === 'edital' ? <EditalPage profileId={workspace.profile.id} subjects={workspace.subjects} topics={workspace.topics} onStudyTopic={focusStudyTopic} /> : null}
+            {page === 'edital' ? <EditalPage profileId={workspace.profile.id} subjects={workspace.subjects} topics={workspace.topics} onStudyTopic={focusStudyTopic} onOpenLeiSeca={() => selectPage('lei-seca')} /> : null}
             {page === 'jurisprudence' ? <JurisprudencePage profileId={workspace.profile.id} /> : null}
             {page === 'lei-seca' ? <LeiSecaPage user={user} profileId={workspace.profile.id} subjects={workspace.subjects} topics={workspace.topics} decks={workspace.decks} onStudyTopic={focusStudyTopic} /> : null}
             {page === 'performance' ? <PerformancePage user={user} profileId={workspace.profile.id} subjects={workspace.subjects} onReviewWrong={startWrongReview} onReviewDue={startDueReview} /> : null}
